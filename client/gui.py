438869cf367bc30cb0cd12dbc3671b27c6a8967d
guoyun2020 <guoyun2000@163.com>
2021-05-24 03:30:17 +0800

    finish GUI

---
+++ client/gui.py
@@ -0,0 +1,128 @@
+import socket
+import numpy
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import cv2
+import sys
+import face_recognition
+from socket_client import sock_client
+from PyQt5 import QtCore, QtWidgets
+import os
+import time
+
+
+class gui(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.initTimer()
+
+    def initTimer(self):
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.show_pic)
+
+
+    def show_pic(self):
+        ret, frame = self.vc.read()
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        reimg=cv2.imread('Unknown.png')
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Only process every other frame of video to save time
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+
+        # Display the results
+        for (top, right, bottom, left) in face_locations:
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            area = (right - left) * (bottom - top)
+
+            face = frame[top:bottom, left:right]
+
+            reimg = sock_client(face, self.info)
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        if not ret:
+            print('read error!\n')
+            return
+        self.timelabel.setText(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'                  当前课程：网络编程')
+        self.timelabel.setFont(QFont("微软雅黑", 10, QFont.Bold))
+        img = cv2.resize(frame, dsize=(491, 271))
+        image_height, image_width, image_depth = img.shape  # 读取图像高宽深度
+        QIm = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        QIm = QImage(QIm.data, image_width, image_height, image_width * image_depth, QImage.Format_RGB888)
+        self.lbl.setPixmap(QPixmap.fromImage(QIm))
+        image_height1, image_width1, image_depth1 = reimg.shape  # 读取图像高宽深度
+        QIm1 = cv2.cvtColor(reimg, cv2.COLOR_BGR2RGB)
+        QIm1 = QImage(QIm1.data, image_width1, image_height1, image_width1 * image_depth1, QImage.Format_RGB888)
+        self.img.setPixmap(QPixmap.fromImage(QIm1))
+
+
+    def openCamera(self):
+        self.lbl.setEnabled(True)
+        self.vc = cv2.VideoCapture(0)
+        self.openCameraBtn.setEnabled(False)
+        self.closeCameraBtn.setEnabled(True)
+        self.timer.start(100)
+
+    def closeCamera(self):
+        self.vc.release()
+        self.openCameraBtn.setEnabled(True)
+        self.closeCameraBtn.setEnabled(False)
+        self.QLable_close()
+        self.timer.stop()
+
+    def initUI(self):
+        self.resize(538, 630)
+        self.lbl = QLabel(self)
+        self.lbl.setGeometry(QtCore.QRect(20, 30, 491, 271))
+        self.timelabel = QLabel(self)
+        self.timelabel.setGeometry(QtCore.QRect(20, 3, 491, 20))
+        self.info = QLabel(self)
+        self.info.setGeometry(QtCore.QRect(230, 330, 160, 280))
+        self.info.setFrameShape(QtWidgets.QFrame.Box)
+        self.info.setStyleSheet('background-color: rgb(255, 255, 255)')
+        self.info.setLineWidth(1)
+        self.img = QLabel(self)
+        self.img.setGeometry(QtCore.QRect(20, 330, 200, 280))
+        self.img.setFrameShape(QtWidgets.QFrame.Box)
+        self.img.setStyleSheet('background-color: rgb(255, 255, 255)')
+        self.img.setLineWidth(1)
+        self.openCameraBtn = QtWidgets.QPushButton(self)
+        self.openCameraBtn.setText('开机')
+        self.openCameraBtn.setGeometry(QtCore.QRect(420, 330, 91, 31))
+        self.openCameraBtn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.openCameraBtn.clicked.connect(self.openCamera)
+        self.closeCameraBtn = QtWidgets.QPushButton(self)
+        self.closeCameraBtn.setText('关机')
+        self.closeCameraBtn.setGeometry(QtCore.QRect(420, 380, 91, 31))
+        self.closeCameraBtn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.closeCameraBtn.clicked.connect(self.closeCamera)
+        self.openCameraBtn.setEnabled(True)
+        self.closeCameraBtn.setEnabled(False)
+        self.QLable_close()
+        self.setWindowTitle('人脸考勤系统 v1.0')
+        self.show()
+
+
+    def QLable_close(self):
+        self.lbl.setStyleSheet("background:black;")
+        self.lbl.setPixmap(QPixmap())
+
+    def start(self):
+        self.timer.start(100)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ex = gui()
+    sys.exit(app.exec_())
--- client/socket_client.py
+++ client/socket_client.py
@@ -4,15 +4,27 @@ import sys
 import struct
 import numpy
 import cv2
+from PyQt5.QtGui import QFont
+
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 15]
 
-def sock_client(src):
+def sock_client(src, info):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('192.168.5.106', 2000))
+        s.connect(('192.168.5.103', 2000))
     except socket.error as msg:
         print(msg)
         print(sys.exit(1))
+
+    def recvall(sock, count):
+        buf = b''  # buf是一个byte类型
+        while count:
+            # 接受TCP套接字的数据。数据以字符串形式返回，count指定要接收的最大数据量.
+            newbuf = sock.recv(count)
+            if not newbuf: return None
+            buf += newbuf
+            count -= len(newbuf)
+        return buf
 
     while True:
         result, imgencode = cv2.imencode('.jpg', src, encode_param)
@@ -27,9 +39,25 @@ def sock_client(src):
         # 发送数据
         s.send(stringData)
 
-        data=s.recv(1024)
-        recv_data = data.decode('utf-8')
-        if recv_data != 'Unknown':
-            print('签到成功:', recv_data)  # 输出我接收的信息
-        break
+        length = recvall(s, 16)  # 获得图片文件的长度,16代表获取长度
+        stringData = recvall(s, int(length))  # 根据获得的文件长度，获取图片文件
+        data = numpy.frombuffer(stringData, numpy.uint8)  # 将获取到的字符流数据转换成1维数组
+        decimg = cv2.imdecode(data, cv2.IMREAD_COLOR)  # 将数组解码成图像
+        data1 = s.recv(1024)
+        recv_data1 = data1.decode('utf-8')
+        data2 = s.recv(1024)
+        recv_data2 = data2.decode('utf-8')
+        data3 = s.recv(1024)
+        recv_data3 = data3.decode('utf-8')
+        data4 = s.recv(1024)
+        recv_data4 = data4.decode('utf-8')
+
+        info.setText('姓名：'+recv_data1+'\n'+'\n'+'学号：'+recv_data2+'\n'+'\n'+'班级：'+recv_data3+'\n'+'\n'+'专业：'+recv_data4+'\n')
+        info.setFont(QFont("微软雅黑", 10, QFont.Bold))
+            # return recv_data
+        if decimg is not None:
+            return decimg
+
+
+
 
